921763852b0f19e7510a366553f3ca69f79a4395
akberahmed7 <153420332+akberahmed7@users.noreply.github.com>
2024-01-04 23:28:23 +0500

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,6 @@
+                        
+base=float(input("Enter the base of triangle"))
+height=float(input("Enter the height of triangle"))
+area=(base*height)/2
+print("the area of triangle is:{area}")
+
---
+++ average of two number.py
@@ -0,0 +1,5 @@
+
+num1= float(input("Enter the first floating-point number"))
+num2= float(input("Enter the second floating-point number"))
+average=(num1+num2)/2
+print(f"The average is:{average:}")
